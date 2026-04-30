09ffe8daf1e2aa2b9d3ed751d0211cce4866a6bf
marcossial <marcossial043@gmail.com>
2026-04-30 08:57:42 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/com/example/biggerbet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.biggerbet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Text("OLA BIGGERBET")
+          ],
+        ),
+      ),
+    );
+  }
+}
